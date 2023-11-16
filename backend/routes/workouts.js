17764dfd742313a55cbd46fdc314a2a50da40fd1
kemerald25 <126842942+kemerald25@users.noreply.github.com>
2023-11-16 14:39:58 -0600

    all set

---
+++ backend/routes/workouts.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const {
+    createWorkout
+} = require('../controllers/workoutController')
+
+const router = express.Router()
+
+
+// Get all workouts
+router.get('/', (req, res) => {
+    res.json({ mssg: 'GET all workouts' })
+})
+
+// Get a single workout
+router.get('/:id', (req, res) => {
+    res.json({ mssg: 'GET a single workout' })
+})
+
+// POST a new workout
+router.post('/', createWorkout)
+
+
+// DELETE a workout
+router.delete('/:id', (req, res) => {
+    res.json({ mssg: 'DELETE a workout' })
+})
+
+// UPDATE a workout
+router.patch('/:id', (req, res) => {
+    res.json({ mssg: 'UPDATE a workout' })
+})
+
+
+module.exports = router
