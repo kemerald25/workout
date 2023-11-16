c20c7f887879b0eba4822f3e96e69b5ed3e92913
kemerald25 <126842942+kemerald25@users.noreply.github.com>
2023-11-16 15:01:42 -0600

    Update workoutController.js

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 // get all workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+    const workouts = await Workout.find({}).sort({ createdAt: -1 })
 
     res.status(200).json(workouts)
 }
@@ -12,41 +12,58 @@ const getWorkouts = async (req, res) => {
 // get a single workout
 
 const getWorkout = async (req, res) => {
-    const { id } =  req.params
+    const { id } = req.params
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status((404)).json({error: 'No such workout'})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status((404)).json({ error: 'No such workout' })
     }
 
     const workout = await Workout.findById(id)
 
-    if(!workout) {
-        return res.status(404).json({error: 'No such workout'})
+    if (!workout) {
+        return res.status(404).json({ error: 'No such workout' })
     }
 
     res.status(200).json(workout)
 }
 
 //create new workout
- const createWorkout = async (req, res) => {
+const createWorkout = async (req, res) => {
     const { title, load, reps } = req.body
 
-     // add doc to db
+    // add doc to db
     try {
         const workout = await Workout.create({ title, load, reps })
         res.status(200).json(workout)
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
- }
+}
 
 
 // delete a workout
+
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status((404)).json({ error: 'No such workout' })
+    }
+
+    const workout = await Workout.findOneAndDelete({_id: id})
+
+    if (!workout) {
+        return res.status(400).json({ error: 'No such workout' })
+    }
+
+    res.status(200).json(workout)
+}
 
 // update a workout
 
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout
 }
