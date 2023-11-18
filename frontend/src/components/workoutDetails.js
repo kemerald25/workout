00203f103120a480407fb854ad88222f2ebfa33d
kemerald25 <126842942+kemerald25@users.noreply.github.com>
2023-11-18 11:57:01 -0600

    almost done

--- frontend/src/components/workoutDetails.js
+++ frontend/src/components/workoutDetails.js
@@ -1,10 +1,24 @@
+import { useWorkoutsContext } from '../hooks/useworkoutsContext'
 const WorkoutDetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext()
+    const handleClick = async () => {
+        const response = await fetch ('/api/workouts/' + workout._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+        alert('Workout deleted')
+
+        if (response.ok) {
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
     return (
         <div className="workout-details" >
             <h4>{workout.title}</h4>
             <p><strong>Load (Kg):</strong> {workout.load} </p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     )
 }
