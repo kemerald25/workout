93a5918d41c834a659e09a1a4200fdbc794883ff
kemerald25 <126842942+kemerald25@users.noreply.github.com>
2023-11-18 09:52:41 -0600

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WorkoutsContextProvider } from './context/workoutContext';
+import { WorkoutsContextProvider } from './context/WorkoutsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutContext.Provider>
+    <WorkoutsContextProvider>
       <App />
-    </WorkoutContext.Provider>
+    </WorkoutsContextProvider>
   </React.StrictMode>
 
 );
